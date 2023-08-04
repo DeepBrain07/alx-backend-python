774eefdbb6bf1e47fd4741897d12ed25d462bae7
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-04 21:18:45 +0100

    Task 6 completed

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This Module defines a python function """
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[int | float]) -> float:
+    """ This function returns the sum of a list """
+    sum: float = 0
+    for x in mxd_lst:
+        sum += x
+    return sum
