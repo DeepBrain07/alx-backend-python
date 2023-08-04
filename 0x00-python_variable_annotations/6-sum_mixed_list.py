b3fd8991c5e9b4a5380c3a0ec07784bedbc32023
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-04 21:20:45 +0100

    Task 6 Debugged

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 import typing
 
 
-def sum_mixed_list(mxd_lst: typing.List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
     """ This function returns the sum of a list """
     sum: float = 0
     for x in mxd_lst:
