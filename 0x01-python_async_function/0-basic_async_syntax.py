02544171d03739c1e9d633bba2623bb4d366459c
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-07 23:10:18 +0100

    Task 0 completed

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module defines a python function """
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """ This function waits for a random delay between 0 and max_delay """
+    randf = random.uniform(0, max_delay + 1)
+    await asyncio.sleep(randf)
+    return randf
