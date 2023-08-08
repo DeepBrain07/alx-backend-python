05538cc4226b5a11c41053bea604c88bc0f3eb5d
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-08 01:07:35 +0100

    Debugged

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,8 +5,5 @@ import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-T = typing.TypeVar('T')
-
-
-def task_wait_random(max_delay: int = 10) -> typing.Type[T]:
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     return asyncio.create_task(wait_random(max_delay))
