05a801e4c2b55b864ab337459f72020411983ea3
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-08 01:13:20 +0100

    Debugged

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,4 +5,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """ This function returns an asyncio class """
     return asyncio.create_task(wait_random(max_delay))
