361799a6472f0a49f5a6d62d26c261354eeb41af
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-09 00:44:37 +0100

    Completed

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module defines a python function """
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List:
+    """ This function returns 10 random numbers """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
