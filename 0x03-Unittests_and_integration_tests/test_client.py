890df5070a5b008ea68f92805bac44b6593ae216
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-28 13:10:54 +0100

    Updating

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,6 +34,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('utils.requests.get')
     def test_public_repos_url(self, mock_get):
         """ Tests the '_public_repos_url' property
+            and 'public_repos' methods
         """
         obj = GithubOrgClient('abc')
         with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)\
@@ -50,3 +51,14 @@ class TestGithubOrgClient(unittest.TestCase):
                 ]
             mock_get.return_value = mock_response
             self.assertEqual(obj.public_repos('key1234'), ['payload1'])
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, arg1, arg2, res):
+        """ Tests 'GithubOrgClient.has_license'
+        """
+        obj = GithubOrgClient('abc')
+        license_check = obj.has_license(arg1, arg2)
+        self.assertEqual(license_check, res)
