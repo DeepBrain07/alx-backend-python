11b0a6e92b2de83d61ec2d44d3683b662b8d07f0
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-08-28 12:51:36 +0100

    Updating

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 import unittest
 from unittest.mock import patch, Mock, PropertyMock
-from utils import get_json
+import utils
 from client import GithubOrgClient
 import client
 from parameterized import parameterized
@@ -31,13 +31,22 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_with(f'https://api.github.com/orgs/{arg}')
         mock_get.assert_called_once()
 
-    def test_public_repos_url(self):
+    @patch('utils.requests.get')
+    def test_public_repos_url(self, mock_get):
         """ Tests the '_public_repos_url' property
         """
         obj = GithubOrgClient('abc')
         with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)\
                 as mock_method:
             mock_method.return_value = \
-                {'name': 'abc', 'repos_url': 'http://repost.url.com'}
+                {'org_name': 'abc', 'repos_url': 'http://repost.url.com'}
             repos_url = obj._public_repos_url
             self.assertEqual(repos_url, mock_method.return_value['repos_url'])
+            mock_response = Mock()
+            mock_response.json.return_value = \
+                [
+                    {'name': 'payload1', 'license': {'key': 'key1234'}},
+                    {'name': 'payload2', 'license': {'key': 'key9876'}}
+                ]
+            mock_get.return_value = mock_response
+            self.assertEqual(obj.public_repos('key1234'), ['payload1'])
